56f9a362e2551d7b0fca35888731ae0a90b61366
jingyuanli001 <33391999+jingyuanli001@users.noreply.github.com>
2022-06-07 20:53:50 -0400

    new folder structure

---
+++ l2r/runners/rl-runner.py
@@ -0,0 +1,16 @@
+
+
+class ReinforcementLearningRunner():
+    def __init__(self):
+        # TODO: initialize environment
+        
+        # TODO: initialize agent
+        
+        # TODO: initialize visual encoder
+        pass
+    
+    def train(self):
+        pass
+    
+    def evaluate(self):
+        pass
---
+++ l2r/runners/sl-runner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  6 22:47:31 2022
+
+@author: jingyuanli
+"""
+
